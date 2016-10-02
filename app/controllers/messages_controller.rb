6a53047074e07c661668af8ea88fd5144e1b36e6
IPPEI IDEI <ippei.idei@gmail.com>
2016-10-02 03:42:45 +0000

    using Bootstrap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
   end
-
+  
   private
   def message_params
     params.require(:message).permit(:name, :body)
